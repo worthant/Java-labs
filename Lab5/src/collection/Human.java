ba13eec47c3092dc463e689b387d61dee2593a5d
Boris <b_dvorkin@niuitmo.ru>
2023-02-08 00:49:44 +0300

    coded Human class

--- Lab5/src/collection/Human.java
+++ Lab5/src/collection/Human.java
@@ -1,4 +1,35 @@
 package collection;
 
-public class Human {
+public class Human implements Comparable<Human> {
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private String zodiacSign;
+
+    public Human(String name, String zodiacSign) {
+        this.name = name;
+        this.zodiacSign = zodiacSign;
+    }
+
+    @Override
+    public int compareTo(Human o) {
+        if (o == null) {
+            return 1;
+        }
+        if (this.name != null) {
+            return this.name.compareTo(o.name);
+        } else {
+            if (this.zodiacSign != null) {
+                return this.zodiacSign.compareTo(o.zodiacSign);
+            } else if (o.zodiacSign != null) {
+                return -o.zodiacSign.compareTo(this.zodiacSign);
+            }
+        }
+        return 0;
+    }
+    
+    @Override
+    public String toString() {
+        return "Human: " +
+                "name = '" + name + "'" +
+                ", zodiacSign = " + zodiacSign;
+    }
 }

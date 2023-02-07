dc0381ed288d56297c75ef1149602fd0ccfb0da9
Boris <b_dvorkin@niuitmo.ru>
2023-02-08 00:12:17 +0300

    added Coordinates in collection

---
+++ Lab5/src/collection/Coordinates.java
@@ -0,0 +1,50 @@
+package collection;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private int x; //Максимальное значение поля: 499
+    private double y; //Значение поля должно быть больше -274
+
+    public Coordinates(int x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates coordinates = (Coordinates) o;
+        return coordinates.x == x && Double.compare(coordinates.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}

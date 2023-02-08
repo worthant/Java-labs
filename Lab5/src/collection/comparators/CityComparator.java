7258f3ef6135f02c1867329157de7465cc6c5d42
Lol <programming@gmail.com>
2023-02-08 10:14:52 +0300

    added CityComparator in cmoparators

---
+++ Lab5/src/collection/comparators/CityComparator.java
@@ -0,0 +1,19 @@
+package collection.comparators;
+
+import collection.City;
+
+import java.util.Comparator;
+
+public class CityComparator implements Comparator<City> {
+    @Override
+    public int compare(City o1, City o2) {
+        if (o1 == null) {
+            if (o2 == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        return o1.compareTo(o2);
+    }
+}

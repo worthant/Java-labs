a394f9d77651698f6ce5df60cea0251b75517b58
worthant <b_dvorkin@niuitmo.ru>
2023-05-15 04:26:55 +0300

    made beautiful auth [front]

--- lab8/client/src/gui/LoginWindow.java
+++ lab8/client/src/gui/LoginWindow.java
@@ -1,8 +1,10 @@
 package gui;
 
 import Client.Client;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -20,6 +22,9 @@ import requestLogic.requestSenders.AuthRequestSender;
 import responses.AuthResponse;
 import serverLogic.ServerConnectionHandler;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class LoginWindow {
 
     private Stage stage;
@@ -27,68 +32,48 @@ public class LoginWindow {
 
     public LoginWindow() {
         stage = new Stage();
-        VBox root = new VBox();
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(20);
-        root.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("loginWindow.fxml")));
+            scene = new Scene(root);
+            Button signInButton = (Button) loader.getNamespace().get("signInButton");
+            signInButton.getStyleClass().add("button-style");
+            TextField usernameField = (TextField) loader.getNamespace().get("usernameField");
+            TextField passwordField = (TextField) loader.getNamespace().get("passwordField");
+            ImageView geoIcon = (ImageView) loader.getNamespace().get("geoIcon");
+            signInButton.setOnAction(event -> {
+                // Get username and password from text fields
+                String username = usernameField.getText().trim();
+                char[] password = passwordField.getText().trim().toCharArray();
 
-        // Geolocation Icon
-        Image image = new Image("C:\\Users\\Boris\\Itmo\\Java_labs\\lab8\\docs\\img.png");
-        ImageView geoIcon = new ImageView(image);
-        geoIcon.setFitHeight(50);
-        geoIcon.setFitWidth(50);
-        geoIcon.setOnMouseClicked(event -> {
-            // Here goes the logic for changing the locale
-        });
+                // Authentication logic
+                try {
+                    AuthRequestSender rqSender = new AuthRequestSender();
+                    AuthResponse response = rqSender.sendAuthData(username, password, ServerConnectionHandler.getCurrentConnection());
 
-        // Welcome Text
-        Text welcomeText = new Text("Welcome back");
-        welcomeText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        Text credentialsText = new Text("Please enter your credentials");
-        credentialsText.setFont(Font.font("Arial", FontWeight.NORMAL, 16));
-
-        // Username and Password Fields
-        TextField usernameField = new TextField();
-        usernameField.setPromptText("Username");
-        TextField passwordField = new TextField();
-        passwordField.setPromptText("Password");
-
-        // Sign In Button
-        Button signInButton = new Button("Sign In");
-        signInButton.setTextFill(Color.GREEN);
-        signInButton.setOnAction(event -> {
-            // Get username and password from text fields
-            String username = usernameField.getText().trim();
-            char[] password = passwordField.getText().trim().toCharArray();
-
-            // Authentication logic
-            try {
-                AuthRequestSender rqSender = new AuthRequestSender();
-                AuthResponse response = rqSender.sendAuthData(username, password, ServerConnectionHandler.getCurrentConnection());
-
-                if (response.isAuth()) {
-                    // If authentication is successful, close the login window and open the collections window
-                    Client.getInstance(username, password);
-                    stage.close();
-                    CollectionsWindow collectionsWindow = new CollectionsWindow();
-                    collectionsWindow.show();
-                } else {
-                    // If authentication fails, show an error message
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setHeaderText(null);
-                    alert.setContentText("There is no user with this name, or password is incorrect");
-                    alert.showAndWait();
+                    if (response.isAuth()) {
+                        // If authentication is successful, close the login window and open the collections window
+                        Client.getInstance(username, password);
+                        stage.close();
+                        CollectionsWindow collectionsWindow = new CollectionsWindow();
+                        collectionsWindow.show();
+                    } else {
+                        // If authentication fails, show an error message
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setHeaderText(null);
+                        alert.setContentText("There is no user with this name, or password is incorrect");
+                        alert.showAndWait();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+            });
 
-        // Adding elements to the layout
-        root.getChildren().addAll(geoIcon, welcomeText, credentialsText, usernameField, passwordField, signInButton);
-
-        scene = new Scene(root, 500, 500);
-        stage.setScene(scene);
+            scene = new Scene(root, 500, 500);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style.css")).toExternalForm());
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void show() {
